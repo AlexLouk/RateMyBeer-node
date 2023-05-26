b357d1b274a9a2d7e737434a0756ea884572d9cd
jimmychanga32 <v.franco222@gmail.com>
2023-05-26 17:03:22 +0200

    routes

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/', router);
 app.use('/about', router)
+app.use('/FAQs', router)
+app.use('/home', router)
+app.use('/login', router)
+app.use('/rating', router)
+app.use('/user', router)
 
 const port = 8000;
 app.listen(port, () => {
