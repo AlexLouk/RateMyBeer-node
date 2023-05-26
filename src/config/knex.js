d3dea978d7e2c0b8052c947a13ef1f1ba47068fb
Alexandros Loukaridis <92loal1bif@hft-stuttgart.de>
2023-05-26 16:00:14 +0200

    neuster stand

---
+++ src/config/knex.js
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'kandula.db.elephantsql.com',
+      port: '5432',
+      user: 'vkzsbicr',
+      password: 'gaQfUop0YHHJbv6_FmRK_0mDjqWrzNgU',
+      database: 'vkzsbicr'
+    },
+
+
+    migrations: {
+      directory: './migrations'
+    },
+    useNullAsDefault: true
+  }
+};
