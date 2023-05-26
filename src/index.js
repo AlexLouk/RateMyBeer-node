ca27a6982fed36abda58735a6635f1980e7f4d15
jimmychanga32 <v.franco222@gmail.com>
2023-05-26 16:28:59 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import { express } from "express";
 
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const router = require('./routes/router')
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/', router);
+app.use('/about', router)
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
