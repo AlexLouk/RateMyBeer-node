899ac64d6746d4bffec7946d42990c5914c043cb
Alexandros Loukaridis <92loal1bif@hft-stuttgart.de>
2023-05-26 15:28:45 +0200

    new project structure

---
+++ scripts/fetchUsers.js
@@ -0,0 +1,23 @@
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host: 'kandula.db.elephantsql.com',
+      port: '5432',
+      user: 'vkzsbicr',
+      password: 'gaQfUop0YHHJbv6_FmRK_0mDjqWrzNgU',
+      database: 'vkzsbicr'
+    },
+    useNullAsDefault: true
+  });
+  
+  knex.select().from('rmb.user')
+    .then(rows => {
+      console.log(rows); 
+    })
+    .catch(error => {
+      console.error(error);
+    })
+    .finally(() => {
+      knex.destroy();
+    });
+  
